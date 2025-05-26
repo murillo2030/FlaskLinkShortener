b9ed3b9dcc9f549c1caf1af9d35698770a144542
Murillo <100052495+MurilloMB@users.noreply.github.com>
2025-05-26 18:04:17 -0300

    Fixing logger bugs

--- Logs.py
+++ Logs.py
@@ -6,7 +6,7 @@ import time
 
 class Log():
     def __init__(self):
-        self.__log_name: str = None 
+        self.__log_name__: str = None 
 
         if not os.path.exists("./logs"):
             os.mkdir("./logs")
@@ -14,12 +14,14 @@ class Log():
     def __update_log_name__(self, operation: str):
         self.__update_prefix__()
         self.__update_suffix__()
-        self.__log_name = "./logs/" + self.__log_prefix + self.__log_suffix + (f'_{ operation }' if operation else '') + '.txt'
+        self.__log_name__ = f"./logs/{ self.__log_prefix__ }_{ self.__log_suffix__ }_{ operation if operation else '' }.txt"
+        print(self.__log_name__)
         
     def create_log(self, message: str, operation: str = None) -> bool:
+        print(self.__log_name__)
         try:
             self.__update_log_name__(operation)
-            with open(self.__log_name, 'x') as fp:
+            with open(self.__log_name__, 'x') as fp:
                 fp.write(message)
             return True
         except FileNotFoundError as e:
@@ -30,13 +32,13 @@ class Log():
             return False
 
     def __update_suffix__(self):
-        self.__log_suffix = datetime.now().strftime("%Y%m%d_%H%M%s")
-        self.__log_suffix = f'{ self.__log_suffix }'
+        self.__log_suffix__ = datetime.now().strftime("%Y%m%d_%H%M%S")
+        self.__log_suffix__ = f'{ self.__log_suffix__ }'
     
     def __update_prefix__(self):
         self.__update_timestamp__()
-        self.__log_prefix = f'logfile_{ self.__timestamp }_'
+        self.__log_prefix__ = f'logfile_{ self.__timestamp__ }'
 
     
     def __update_timestamp__(self):
-        self.__timestamp = int(time.time())
+        self.__timestamp__ = int(time.time())
