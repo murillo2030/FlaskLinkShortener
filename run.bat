0cafb42652e01c163104ac276ab0960a84b7f561
Murillo <murillo.mb21@gmail.com>
2025-08-20 20:38:40 -0300

    Improved run executables

--- run.bat
+++ run.bat
@@ -1,1 +1,6 @@
-waitress-serve --listen=0.0.0.0:8080 app:app
+if exist .\\database.enc (
+    waitress-serve --listen=0.0.0.0:8080 app:app
+) else (
+    flask initdb
+    waitress-serve --listen=0.0.0.0:8080 app:app
+)
