e95ac5e47585ac4dcebb4366b2879298ba6fef77
murillo2030 <murillo.mb21@gmail.com>
2025-10-25 18:29:09 -0300

    Updated run

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 if exist .\\database.db (
-    waitress-serve --listen=0.0.0.0:8080 app:app
+    waitress-serve app:app
 ) else (
     flask initdb
-    waitress-serve --listen=0.0.0.0:8080 app:app
+    waitress-serve app:app
 )
